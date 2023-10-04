6b80e84bcf5476f578d2db9a2431b0ae4426f671
octavioegeafaure <octavioegeaf@gmail.com>
2023-10-04 15:00:32 -0300

    cambios header bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Home } from './assets/Home/Home'
-
+import { HashRouter } from "react-router-dom";
 function App() {
   
 
   return (
     <>
+    <HashRouter>
       <Home />
+    </HashRouter>
+      
     </>
   )
 }
