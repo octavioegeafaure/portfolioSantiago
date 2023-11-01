adaebd40416e907f739d728715e9fac819e7c255
octavioegeafaure <octavioegeaf@gmail.com>
2023-11-01 13:59:08 -0300

    se agrega loader en home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,58 @@
+import { useEffect } from "react"
+import { useState } from "react"
 import { Banner } from "./Banner/Banner"
 import { Header } from "./Header/Header"
+import { motion } from "framer-motion"
+
 import "./Home.css"
 export const Home = () => {
+  
+const [loaderAnimationComplete, setLoaderAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    // Use a setTimeout to mark the animation as complete
+    setTimeout(() => {
+      setLoaderAnimationComplete(true);
+    }, 1000); // Adjust the duration based on your animation's duration
+  }, []);
+
   return (
     <div className="home">
-        <Header />
-        <Banner />
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ delay: 1.2, duration: 2.2 }}
+        variants={{
+          visible: { opacity: 0 },
+          hidden: { opacity: 1 },
+        }}
+        className={`home-loader ${loaderAnimationComplete ? 'animation-complete' : ''}`}
+      >
+        <div className="home-loader-texto">
+          <div className="home-loader-overflow">
+            <motion.p
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ delay: 1.5, duration: 1.4 }}
+            variants={{
+            visible: { y: -50, opacity: 1 },
+            hidden: { y: 0, opacity: 1 },
+            }}
+          >
+          santiago espeche
+          </motion.p> 
+          </div>
+
+        </div>
+
+      </motion.div>
+
+      {/* Render Header and Banner components */}
+      <Header className={`header ${loaderAnimationComplete ? 'animation-complete' : ''}`} />
+      <Banner className={`banner ${loaderAnimationComplete ? 'animation-complete' : ''}`} />
     </div>
+    
   )
 }
