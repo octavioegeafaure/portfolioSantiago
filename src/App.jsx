faf32408f96ea736b26e666daa2037a0b1983539
octavioegeafaure <octavioegeaf@gmail.com>
2023-11-27 15:09:51 -0300

    se cambia enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,17 @@ import {Projects} from './Projects/Projects';
 import {ProjectsDetalle} from './ProjectsDetalle/ProjectsDetalle';
 import { About } from './About/About';
 
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <>
-    <Switch>
+    <HashRouter>
         <Route path="/portfolioSantiago"  ><Home /></Route>
         <Route path="/portfolioSantiago/projects/"  ><Projects /></Route>
         <Route path="/portfolioSantiago/projects/:id"  ><ProjectsDetalle /></Route>
         <Route path="/portfolioSantiago/about/"  ><About /></Route>
         <Route path="/portfolioSantiago/contact/"  ><Footer /></Route>
-        
-
-    </Switch>
-   
-        {/* <Route path="/portfolioSantiago/projects/:id" component={ProjectsDetalle} /> */}
-        
-      
+    </HashRouter>   
     </>
   )
 }
